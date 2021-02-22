44024f054d4671447b3213f01f37843e55906aa6
Keith Frazier <keithers98@gmail.com>
2021-02-21 17:22:17 -0800

    Added test cases for Polybius Square

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -3,11 +3,38 @@ const {expect} = require("chai")
 
 describe("polybius", () =>{
     it("should return a string", () =>{
-        const actual = polybius("Hello!")
+        const actual = polybius("hello")
         expect(actual).to.be.string
     })
-    it("should return false if string length is uneven (exluding spaces)", () =>{
-        const actual = polybius("11411")
+    it("should return false if string length is uneven when decoding(exluding spaces)", () =>{
+        const actual = polybius("11411", false)
         expect(actual).to.be.false
     })
+    it("should encode I and J to 42",() =>{
+        const actual = polybius("ij")
+        expect(actual).to.equal("4242")
+    })
+    it("should decode I and J appropriately"), () =>{
+        const actual = polybius("42", false)
+        expect(actual).to.equal("(i/j)")
+    }
+    it("should maintain spaces when encoding"),() =>{
+        const encode = polybius("test case")
+        expect(encode).to.equal("44513444 31113451")
+    }
+
+    it("should maintain spaces when decoding", () =>{
+        const decode = polybius("44513444 31113451", false)
+        expect(decode).to.equal("test case")
+    })
+
+    it("should ignore capital letters", () => {
+        const actual = polybius("CAPITAL")
+        expect(actual).to.equal("31115342441113")
+    })
+
+    it("should ignore special characters"), () => {
+        const actual = polybius("hello!!")
+        expect(actual).to.equal("3251131343!!")
+    }
 })
